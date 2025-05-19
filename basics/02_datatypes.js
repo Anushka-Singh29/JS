b56502b537f7d202a1b340cf5a98281e04d8ceed
Anushka <118214813+Anushka-Singh29@users.noreply.github.com>
2025-05-19 00:19:40 +0000

    Added datatypes file

---
+++ basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version of javascript 
+
+// alert(3+3) //we are using nodejs and not browser hence alert won't work here
+
+console.log(3+2) 
+console.log("VSCode");
+
+let name = "ansuhka"
+let age = 18
+let isLoggedIn = false
+
+/* 
+Primitive datatypes
+number =>2 to the power of 53
+bigInt used for big numbers: reddit facebook, stock market
+string => ""
+boolean => true/false
+null => standalone vale hai
+undefined => abhi value assign nahi hui hai
+symbol => used to find uniqueness
+*/
+console.log(typeof undefined); undefined
+console.log(typeof null); object
