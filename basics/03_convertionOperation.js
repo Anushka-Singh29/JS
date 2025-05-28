eb8de637b329d3f20aa861f516c79005a65e9108
Anushka <118214813+Anushka-Singh29@users.noreply.github.com>
2025-05-28 08:55:40 +0000

    Comparison added

--- basics/03_convertionOperation.js
+++ basics/03_convertionOperation.js
@@ -28,6 +28,26 @@
 // console.log(typeof (num))
 // console.log(typeof stringnumber)
 
-console.log("1"+2)
-console.log(1+2)
-console.log(1+2+"2")
+// console.log("1"+2)
+// console.log(1+2)
+// console.log(1+2+"2")
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter=100;
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter);
+
+let x = 4;
+let y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+let x1 =4;
+const y1= ++x1;
+
+console.log(`x1:${x1}, y1:${y1}`);
+//y1: 4 x1:5
