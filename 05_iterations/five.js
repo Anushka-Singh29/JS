32c0892b6542ac6baa4dc6f3f0ce68517154868c
Anushka <118214813+Anushka-Singh29@users.noreply.github.com>
2025-06-20 20:35:36 +0000

    For Each loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,42 @@
+const coding = ['js', 'ruby', 'java', 'Python', 'go']
+
+coding.forEach(function (item) {
+    // console.log(item);
+})
+
+coding.forEach((val) => {
+    // console.log(val);
+
+})
+
+
+// function printme(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printme)
+
+// coding.forEach((item, index, arr)=>{
+//     console.log(item, index, arr);
+
+// })
+
+const mycoding = [  
+    {
+        languagename: "javascript",
+        lagunagefilename:"js"
+    },
+    {
+        languagename:"java",
+        lagunagefilename:"java"
+    },
+    {
+        languagename:"python",
+        lagunagefilename:"py"
+    }
+]
+
+mycoding.forEach((eachobj)=>{
+console.log(eachobj);
+
+})
