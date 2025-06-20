514fed53e9cb0ed210366cfc6acdf10d7a1f462b
Anushka <118214813+Anushka-Singh29@users.noreply.github.com>
2025-06-20 19:41:19 +0000

    iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,33 @@
+//for
+
+for (let i = 0; i <= 10; i++) {
+    const e =i;
+    if (e == 5) {
+        //console.log("5 is best number")
+    }
+    //console.log(e)
+    
+}
+
+
+//table of numbers
+// for (let i = 11; i <= 20; i++) {
+//     console.log(`outer loop: ${i}`)
+//     for (let j = 1; j <=10; j++) {
+//     // console.log(`inner loop: ${j} and i value loop: ${i}`)  
+//     console.log(i + '*' + j + ' = ' + i*j);
+//     }
+    
+// }
+
+let myarr = ["flash", "batman", "superman"]
+// console.log(myarr.length);
+
+
+for (let index = 0; index <= myarr.length; index++) {
+    const element = myarr[index];
+    // console.log(element)
+    
+}
+
+//
